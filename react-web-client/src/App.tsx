7b8992900c31404e3ff7afd9b45b56f6fd3a7561
Preetham-Rai <raipreetham8@gmail.com>
2026-03-08 22:15:01 +0530

    Removed demo code

--- react-web-client/src/App.tsx
+++ react-web-client/src/App.tsx
@@ -1,16 +1,10 @@
 import { Outlet } from "react-router";
-import { DatePicker, Switch, Layout } from "antd";
-import { useAppSelector, useAppDispatch } from "./app/hooks";
-import { toggleTheme } from "./store/themeSlice";
+import { Layout } from "antd";
+import { useAppSelector } from "./app/hooks";
 import { useEffect } from "react";
 
 function App() {
-  const dispatch = useAppDispatch();
   const theme = useAppSelector((state) => state.theme.mode);
-
-  const handleThemeChanger = () => {
-    dispatch(toggleTheme());
-  };
 
   useEffect(() => {
     document.querySelector(".main")?.setAttribute("data-theme", theme);
@@ -18,8 +12,6 @@ function App() {
 
   return (
     <Layout className="main">
-      {/* <Switch style={{ width: "20px" }} onChange={handleThemeChanger} />
-      <DatePicker style={{ width: "200px" }} /> */}
       <Outlet />
     </Layout>
   );
